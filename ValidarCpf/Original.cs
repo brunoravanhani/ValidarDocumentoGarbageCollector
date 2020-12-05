b3bdef23fd99c6430929cd436d4ae1c9f6d9501e
Bruno Ferreira Ravanhani <bruno.ravanhani@outlook.com>
2020-12-05 15:48:34 -0400

    initial Commit

---
+++ ValidarCpf/Original.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidarCpf
+{
+    public static class Original
+    {
+        public static bool ValidarCPF(string sourceCPF)
+        {
+            if (String.IsNullOrWhiteSpace(sourceCPF))
+                return false;
+
+            string clearCPF;
+            clearCPF = sourceCPF.Trim();
+            clearCPF = clearCPF.Replace("-", "");
+            clearCPF = clearCPF.Replace(".", "");
+
+            if (clearCPF.Length != 11)
+            {
+                return false;
+            }
+
+            int[] cpfArray;
+            int totalDigitoI = 0;
+            int totalDigitoII = 0;
+            int modI;
+            int modII;
+
+            if (clearCPF.Equals("00000000000") ||
+                clearCPF.Equals("11111111111") ||
+                clearCPF.Equals("22222222222") ||
+                clearCPF.Equals("33333333333") ||
+                clearCPF.Equals("44444444444") ||
+                clearCPF.Equals("55555555555") ||
+                clearCPF.Equals("66666666666") ||
+                clearCPF.Equals("77777777777") ||
+                clearCPF.Equals("88888888888") ||
+                clearCPF.Equals("99999999999"))
+            {
+                return false;
+            }
+
+            foreach (char c in clearCPF)
+            {
+                if (!char.IsNumber(c))
+                {
+                    return false;
+                }
+            }
+
+            cpfArray = new int[11];
+            for (int i = 0; i < clearCPF.Length; i++)
+            {
+                cpfArray[i] = int.Parse(clearCPF[i].ToString());
+            }
+
+            for (int posicao = 0; posicao < cpfArray.Length - 2; posicao++)
+            {
+                totalDigitoI += cpfArray[posicao] * (10 - posicao);
+                totalDigitoII += cpfArray[posicao] * (11 - posicao);
+            }
+
+            modI = totalDigitoI % 11;
+            if (modI < 2) { modI = 0; }
+            else { modI = 11 - modI; }
+
+            if (cpfArray[9] != modI)
+            {
+                return false;
+            }
+
+            totalDigitoII += modI * 2;
+
+            modII = totalDigitoII % 11;
+            if (modII < 2) { modII = 0; }
+            else { modII = 11 - modII; }
+            if (cpfArray[10] != modII)
+            {
+                return false;
+            }
+            // CPF Válido!
+            return true;
+        }
+    }
+}
---
+++ ValidarCpf/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ValidarCpf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            var before2 = GC.CollectionCount(2);
+            var before1 = GC.CollectionCount(1);
+            var before0 = GC.CollectionCount(0);
+            Func<string, bool> sut = Validador.ValidarCPF;
+
+            sw.Start();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                if (!sut("771.189.500-33"))
+                {
+                    throw new Exception("Error!");
+                }
+
+                if (sut("771.189.500-34"))
+                {
+                    throw new Exception("Error!");
+                }
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Tempo total: {sw.ElapsedMilliseconds}ms");
+            Console.WriteLine($"GC Gen #2  : {GC.CollectionCount(2) - before2}");
+            Console.WriteLine($"GC Gen #1  : {GC.CollectionCount(1) - before1}");
+            Console.WriteLine($"GC Gen #0  : {GC.CollectionCount(0) - before0}");
+            Console.WriteLine("Done!");
+        }
+    }
+}
