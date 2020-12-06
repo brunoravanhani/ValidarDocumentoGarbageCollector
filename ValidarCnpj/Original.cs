207ddf09857110f08438677718b6aa77e435fa08
Bruno Ferreira Ravanhani <bruno.ravanhani@outlook.com>
2020-12-05 22:21:30 -0400

    add cnpj original

---
+++ ValidarCnpj/Original.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidarCnpj
+{
+    public static class Original
+    {
+        public static bool ValidarCNPJ(string cnpj)
+        {
+            if (String.IsNullOrWhiteSpace(cnpj))
+                return false;
+
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+
+            if (cnpj.Length != 14)
+                return false;
+
+            int[] multiplicador1 = new[] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new[] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+
+            // Verifica os Patterns mais Comuns para CNPJ's Inválidos
+            if (cnpj.Equals("00000000000000") ||
+                cnpj.Equals("11111111111111") ||
+                cnpj.Equals("22222222222222") ||
+                cnpj.Equals("33333333333333") ||
+                cnpj.Equals("44444444444444") ||
+                cnpj.Equals("55555555555555") ||
+                cnpj.Equals("66666666666666") ||
+                cnpj.Equals("77777777777777") ||
+                cnpj.Equals("88888888888888") ||
+                cnpj.Equals("99999999999999"))
+            {
+                return false;
+            }
+
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+
+            resto = (soma % 11);
+
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+
+            resto = (soma % 11);
+
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+
+            return cnpj.EndsWith(digito);
+        }
+    }
+}
--- ValidarCnpj/Program.cs
+++ ValidarCnpj/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ValidarCnpj
 {
@@ -6,7 +7,31 @@ namespace ValidarCnpj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sw = new Stopwatch();
+            var before2 = GC.CollectionCount(2);
+            var before1 = GC.CollectionCount(1);
+            var before0 = GC.CollectionCount(0);
+
+            sw.Start();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                if (!Validador.ValidarCNPJ("54.046.739/0001-63"))
+                {
+                    throw new Exception("Error!");
+                }
+
+                if (Validador.ValidarCNPJ("54.046.739/0001-64"))
+                {
+                    throw new Exception("Error!");
+                }
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Tempo total: {sw.ElapsedMilliseconds}ms");
+            Console.WriteLine($"GC Gen #2  : {GC.CollectionCount(2) - before2}");
+            Console.WriteLine($"GC Gen #1  : {GC.CollectionCount(1) - before1}");
+            Console.WriteLine($"GC Gen #0  : {GC.CollectionCount(0) - before0}");
+            Console.WriteLine("Done!");
         }
     }
 }
